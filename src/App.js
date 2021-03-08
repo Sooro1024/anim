86a8eb2226742011d3e930da78ce8c8f5b7d32b8
sooro1024 <soorogorgoyan@gmail.com>
2021-03-08 22:49:47 +0400

    second

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import { useState } from "react";
+import { useState, memo, useRef } from "react";
 import "./App.scss";
 
+export const App2 = memo(() => {
+  console.log("object");
+  return <div>sami</div>;
+});
+let ankap;
 function App() {
   const [count, setCount] = useState(0);
-
+  const ref = useRef(null);
   return (
     <div className="App">
       <div className="rect">
-        <div key={Date.now()} className="circle a0" />
+        <div ref={ref} className="circle a0">
+          <App2 />
+        </div>
       </div>
       <button
         onClick={() => {
@@ -22,6 +29,9 @@ function App() {
             "--initial2",
             `rotate(${count * 90 + 1170}deg)`
           );
+          ref.current.classList.remove("a0");
+          ankap = ref.current.offsetWidth; // trigger a DOM reflow
+          ref.current.classList.add("a0");
         }}
       >
         set counte2 {count}
