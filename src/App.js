31b33342cffc459fed0a056905338081e645191c
sooro1024 <soorogorgoyan@gmail.com>
2021-03-08 22:35:56 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.scss";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="rect">
+        <div key={Date.now()} className="circle a0" />
+      </div>
+      <button
+        onClick={() => {
+          setCount((prev) => {
+            return ++prev;
+          });
+          document.documentElement.style.setProperty(
+            "--initial1",
+            `rotate(${count * 90}deg)`
+          );
+          document.documentElement.style.setProperty(
+            "--initial2",
+            `rotate(${count * 90 + 1170}deg)`
+          );
+        }}
+      >
+        set counte2 {count}
+      </button>
     </div>
   );
 }
